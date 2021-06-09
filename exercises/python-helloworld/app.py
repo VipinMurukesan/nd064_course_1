7a7e323f8a9888a747701182547d309ab62b508e
Vipin <vipinm2020@gmail.com>
2021-06-09 09:04:10 +0530

    Add logging

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import json
 
+import logging
 
 app = Flask(__name__)
 
@@ -12,6 +13,10 @@ def status():
     status=200,
     mimetype='application/json'
     )
+    
+    ##log 
+    app.logger.info("Status Request was successfull")
+    
     return response
 
 @app.route("/metrics")
@@ -21,21 +26,23 @@ def metrics():
     status=200,
     mimetype='application/json'
     )
+    
+    app.logger.info("Metrics request was successfull")
+    
     return response
     
 
 
 @app.route("/")
 def hello():
+    app.logger.info("Main request successfull")
     return "Hello World!"
 
 if __name__ == "__main__":
+    logging.basicConfig(filename='app.log',level=logging.DEBUG)
     app.run(host='0.0.0.0')
 
 
-@app.route("/")
-def hello():
-    return "Hello World!"
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+
+
