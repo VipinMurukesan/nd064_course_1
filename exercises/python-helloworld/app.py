d735126f2eda56659435c12098f0f762d5e1640c
Vipin <vipinm2020@gmail.com>
2021-06-09 08:49:03 +0530

    status and metrics end points added

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,5 +1,37 @@
 from flask import Flask
+from flask import json
+
+
 app = Flask(__name__)
+
+
+@app.route("/status")
+def status():
+    response=app.response_class(
+    response=json.dumps({"result":"Ok-healthy"}),
+    status=200,
+    mimetype='application/json'
+    )
+    return response
+
+@app.route("/metrics")
+def metrics():
+    response=app.response_class(
+    response=json.dumps({"status":"success","code":0,"data":{"Usercount":140,"UserCountActive":23}}),
+    status=200,
+    mimetype='application/json'
+    )
+    return response
+    
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
+
 
 @app.route("/")
 def hello():
